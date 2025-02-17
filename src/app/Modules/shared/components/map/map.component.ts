51019a8fa4f7bf3f8d78341ee0a77cd6db12628e
MaiSabeq <168957411+MaiSabeq@users.noreply.github.com>
2025-02-17 11:44:21 +0200

    #ui

--- src/app/Modules/shared/components/map/map.component.ts
+++ src/app/Modules/shared/components/map/map.component.ts
@@ -1,23 +1,52 @@
-import { Component, ElementRef, NgZone, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, Injector, Input, NgZone, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import * as echarts from 'echarts';
 import * as am5 from "@amcharts/amcharts5";
 import * as am5map from "@amcharts/amcharts5/map";
 import am5geodataWorldLow from "@amcharts/amcharts5-geodata/worldLow";
+import { BaseComponent } from '../../base.component';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent  implements OnInit, OnDestroy {
+export class MapComponent extends BaseComponent {
   private root!: am5.Root;
+  @Input() data = [];
+  @Input() animated: boolean = true;
 
-  constructor(private elementRef: ElementRef, private ngZone: NgZone) {}
+  constructor(inject: Injector, private elementRef: ElementRef, private ngZone: NgZone) {
+    super(inject)
+  }
 
-  ngOnInit(): void {
+  protected override ngSuperOnChanges(changes: SimpleChanges) {
+  }
+  protected override ngSuperOnInit() {
+    this.createChart()
+
+  }
+
+  createChart() {
+
     this.ngZone.runOutsideAngular(() => {
-      let root = am5.Root.new(this.elementRef.nativeElement.querySelector("#mapdiv"));
+      let data:any[] = [];
+      const _self = this;
 
+      // Prepare data for the chart
+      this.data.forEach((element:any) => {
+          data.push({
+            subjectId:element.subjectId,
+            id: element.xKey,
+            country: element.xKey,
+            name: element.xKey,
+            value: Number(element.xValue),
+            bulletSettings: { fill: element.color },
+            extraData: element.extraData
+          });
+
+      });
+      
+      let root = am5.Root.new(this.elementRef.nativeElement.querySelector("#mapdiv"));
       let chart = root.container.children.push(
         am5map.MapChart.new(root, {
           panX: "rotateX",
@@ -31,6 +60,58 @@ export class MapComponent  implements OnInit, OnDestroy {
           geoJSON: am5geodataWorldLow
         })
       );
+      let pointSeries = chart.series.push(am5map.MapPointSeries.new(root, {
+        polygonIdField: "country"
+      }));
+      pointSeries.data.setAll(data);
+
+      pointSeries.bullets.push(function (root, series, dataItem) {
+        let container = am5.Container.new(root, {});
+        // if (_self.animated) {
+          let circle = container.children.push(
+            am5.Circle.new(root, {
+              radius: 4,
+              tooltipY: 0,
+              templateField: "bulletSettings",
+              strokeOpacity: 0
+            })
+          );
+          let circle2 = container.children.push(
+            am5.Circle.new(root, {
+              radius: 4,
+              tooltipY: 0,
+              templateField: "bulletSettings",
+              strokeOpacity: 0
+            })
+          );
+
+          circle.animate({
+            key: "scale",
+            from: 1,
+            to: 5,
+            duration: 600,
+            easing: am5.ease.out(am5.ease.cubic),
+            loops: Infinity
+          });
+          circle.animate({
+            key: "opacity",
+            from: 1,
+            to: 0.1,
+            duration: 600,
+            easing: am5.ease.out(am5.ease.cubic),
+            loops: Infinity
+          });
+        // } else {
+
+
+        // }
+
+
+
+        return am5.Bullet.new(root, {
+          sprite: container,
+        });
+      });
 
       polygonSeries.mapPolygons.template.setAll({
         tooltipText: "{name}",
@@ -41,10 +122,15 @@ export class MapComponent  implements OnInit, OnDestroy {
       this.root = root;
     });
   }
-
-  ngOnDestroy(): void {
+  protected override ngSuperAfterViewInit() {
+  }
+  protected override ngSuperOnDestroy() {
     if (this.root) {
       this.root.dispose();
     }
   }
+
+
+
+
 }
